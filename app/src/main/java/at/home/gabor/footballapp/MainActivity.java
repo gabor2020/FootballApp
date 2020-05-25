b084417afa0ff0f17f47239aa4cff89ba9369b91
Gabor <gabor.antal@inode.at>
2020-05-25 15:58:02 +0200

    Daily Commit 25.05.2020 Top5

--- app/src/main/java/at/home/gabor/footballapp/MainActivity.java
+++ app/src/main/java/at/home/gabor/footballapp/MainActivity.java
@@ -1,9 +1,13 @@
 package at.home.gabor.footballapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,4 +27,18 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, TopActivity.class);
         startActivity(intent);
     }
+
+    public void startMedia(View view) {
+        Intent intent = new Intent(this, MediaActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.test_menu, menu);
+        return true;
+    }
+
+
 }
--- app/src/main/java/at/home/gabor/footballapp/TopActivity.java
+++ app/src/main/java/at/home/gabor/footballapp/TopActivity.java
@@ -3,8 +3,13 @@ package at.home.gabor.footballapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class TopActivity extends AppCompatActivity {
 
@@ -12,11 +17,75 @@ public class TopActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_top);
+
+        Spinner spinner = findViewById(R.id.spinner1);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.top_players, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                TextView textName = findViewById(R.id.textViewTopPlayerName);
+                TextView text = findViewById(R.id.textViewTopPlayerDescpription);
+                ImageView image = findViewById(R.id.imageViewTopPlayerImage);
+                switch (position) {
+                    case 0:
+                        Toast.makeText(TopActivity.this, R.string.player_not_selected, Toast.LENGTH_SHORT).show();
+                        image.setVisibility(View.INVISIBLE);
+                        text.setVisibility(View.INVISIBLE);
+                        textName.setVisibility(View.INVISIBLE);
+                        break;
+                    case 1:
+                        image.setImageResource(R.drawable.playerimg1puskas);
+                        image.setVisibility(View.VISIBLE);
+                        text.setText(R.string.description_puskas);
+                        text.setVisibility(View.VISIBLE);
+                        textName.setText(R.string.player_puskas);
+                        textName.setVisibility(View.VISIBLE);
+                        break;
+                    case 2:
+                        image.setImageResource(R.drawable.playerimg2pele);
+                        image.setVisibility(View.VISIBLE);
+                        text.setText(R.string.description_pele);
+                        text.setVisibility(View.VISIBLE);
+                        textName.setText(R.string.player_pele);
+                        textName.setVisibility(View.VISIBLE);
+                        break;
+                    case 3:
+                        image.setImageResource(R.drawable.playerimg3maradona);
+                        image.setVisibility(View.VISIBLE);
+                        text.setText(R.string.description_maradona);
+                        text.setVisibility(View.VISIBLE);
+                        textName.setText(R.string.player_maradona);
+                        textName.setVisibility(View.VISIBLE);
+                        break;
+                    case 4:
+                        image.setImageResource(R.drawable.playerimg4ronaldo);
+                        image.setVisibility(View.VISIBLE);
+                        text.setText(R.string.description_ronaldo);
+                        text.setVisibility(View.VISIBLE);
+                        textName.setText(R.string.player_ronaldo);
+                        textName.setVisibility(View.VISIBLE);
+                        break;
+                    case 5:
+                        image.setImageResource(R.drawable.playerimg5messi);
+                        image.setVisibility(View.VISIBLE);
+                        text.setText(R.string.description_messi);
+                        text.setVisibility(View.VISIBLE);
+                        textName.setText(R.string.player_messi);
+                        textName.setVisibility(View.VISIBLE);
+                        break;
+
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
-
-
-
-
 
 
 }
