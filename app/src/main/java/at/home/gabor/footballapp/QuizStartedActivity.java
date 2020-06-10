65263ebd69e7057e0e9b6e6428f1ef182f3607b4
Gabor <gabor.antal@inode.at>
2020-06-10 23:55:35 +0200

    Daily commit 10.06.2020 Classes created for SQLight

--- app/src/main/java/at/home/gabor/footballapp/QuizStartedActivity.java
+++ app/src/main/java/at/home/gabor/footballapp/QuizStartedActivity.java
@@ -5,8 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class QuizStartedActivity extends AppCompatActivity {
+    private TextView textViewQuestion;
+    private TextView textViewScore;
+    private TextView textViewQuestionCount;
+    private TextView textViewCountDown;
+    private RadioGroup rbGroup;
+    private RadioButton rb1;
+    private RadioButton rb2;
+    private RadioButton rb3;
+    private Button buttonConfirmNext;
+
+    private List<Question> questionList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +31,8 @@ public class QuizStartedActivity extends AppCompatActivity {
         setContentView(R.layout.activity_quiz_started);
     }
 
-    public void startResultActivity(View view) {
+    /*public void startResultActivity(View view) {
         Intent intent = new Intent(this, ResultActivity.class);
         startActivity(intent);
-    }
+    }*/
 }
