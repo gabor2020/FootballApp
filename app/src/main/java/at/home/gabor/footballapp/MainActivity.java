2f1f1418a7374093ac72536a4a549ecc1ce42f1a
Gabor <gabor.antal@inode.at>
2020-06-17 23:56:32 +0200

    Daily Commit 17.06.2020 Navigation drawer added to MainActivity

--- app/src/main/java/at/home/gabor/footballapp/MainActivity.java
+++ app/src/main/java/at/home/gabor/footballapp/MainActivity.java
@@ -12,37 +12,45 @@ import android.view.MenuItem;
 import android.view.View;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static final String SHARED_PREFS = "sharedPrefs";
     public static final String KEY_LANGUAGE = "keyLanguage";
+
+
+    DrawerLayout drawer;
+    NavigationView navigationView;
+    Toolbar toolbar=null;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         loadPrefs();
-        setContentView(R.layout.activity_main);
-    }
+        setContentView(R.layout.activity_home);
 
 
-    public void startEra(View view) {
-        Intent intent = new Intent(this, EraActivity.class);
-        startActivity(intent);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle (
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
 
-    public void startTop(View view) {
-        Intent intent = new Intent(this, TopActivity.class);
-        startActivity(intent);
-    }
-
-    public void startMedia(View view) {
-        Intent intent = new Intent(this, MediaActivity.class);
-        startActivity(intent);
-    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -62,30 +70,6 @@ public class MainActivity extends AppCompatActivity {
                 break;
             case R.id.itemHu:
                 setHungarian();
-                break;
-            case R.id.itemMain1:
-                Intent intent0 = new Intent(this, MainActivity.class);
-                startActivity(intent0);
-                break;
-            case R.id.itemEra1:
-                Intent intent = new Intent(this, EraActivity.class);
-                startActivity(intent);
-                break;
-            case R.id.itemTop51:
-                Intent intent2 = new Intent(this, TopActivity.class);
-                startActivity(intent2);
-                break;
-            case R.id.itemMedia1:
-                Intent intent3 = new Intent(this, MediaActivity.class);
-                startActivity(intent3);
-                break;
-            case R.id.itemQuiz1:
-                Intent intent4 = new Intent(this, QuizActivity.class);
-                startActivity(intent4);
-                break;
-            case R.id.itemDrawerTest1:
-                Intent intent5 = new Intent(this, Home.class);
-                startActivity(intent5);
                 break;
         }
         return true;
@@ -125,7 +109,7 @@ public class MainActivity extends AppCompatActivity {
         overridePendingTransition(0, 0);
     }
 
-    public void loadPrefs (){
+    public void loadPrefs() {
         SharedPreferences prefs = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         localeCode = prefs.getString(KEY_LANGUAGE, "de");
         Resources res = getResources();
@@ -136,4 +120,48 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        // Handle navigation view item clicks here.
+        //here is the main place where we need to work on.
+        int id = item.getItemId();
+        switch (id) {
+
+            case R.id.nav_home:
+                Intent h = new Intent(MainActivity.this, MainActivity.class);
+                startActivity(h);
+                break;
+            case R.id.nav_era:
+                Intent i = new Intent(MainActivity.this, EraActivity.class);
+                startActivity(i);
+                break;
+            case R.id.nav_top5:
+                Intent s = new Intent(MainActivity.this, TopActivity.class);
+                startActivity(s);
+                break;
+            case R.id.nav_media:
+                Intent g = new Intent(MainActivity.this, MediaActivity.class);
+                startActivity(g);
+                break;
+            case R.id.nav_quiz:
+                Intent t = new Intent(MainActivity.this, QuizActivity.class);
+                startActivity(t);
+                break;
+        }
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
+
